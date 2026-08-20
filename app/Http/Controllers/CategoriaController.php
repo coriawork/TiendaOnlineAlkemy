8b215b5d021537cbfc269f09783882f236f0dfd5
coriawork <manuelcoriawork@gmail.com>
2026-08-20 12:51:55 -0300

    Feat: Creacion del resource controller de categoria

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -3,8 +3,68 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Categoria;
 class CategoriaController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categorias = Categoria::all();
+        return response()->json($categorias, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nombre' => 'required|string|max:100',
+            'descripcion' => 'nullable|string',
+        ]);
+
+        $categoria = Categoria::create($request->all());
+        return response()->json($categoria, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Categoria $categoria)
+    {
+        return response()->json($categoria, 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Categoria $categoria)
+    {   
+        return response()->json($categoria, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Categoria $categoria)
+    {
+        $request->validate([
+            'nombre' => 'required|string|max:100',
+            'descripcion' => 'nullable|string',
+        ]);
+
+        $categoria->update($request->all());
+        return response()->json($categoria, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Categoria $categoria)
+    {
+        $categoria->delete();
+        return response()->json(null, 204);
+    }
 }
